7dc14b11d8c162ce50582c00ee7ec587a6c69d8b
Diogo-Barboza <diogorodriguesbb@gmail.com>
2026-03-20 21:03:02 -0300

    engine geradora de treino

--- workoutengine/admin.py
+++ workoutengine/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Exercicio
+from .models import Exercicio, Treino, ItemTreino
 
 # Register your models here.
 
@@ -8,3 +8,19 @@ class ExerciciosAdmin(admin.ModelAdmin):
     list_display = ('name', 'muscle_group', 'difficulty')
     list_filter = ('muscle_group', 'difficulty')
     search_fields = ('name',)
+
+class WorkoutItemInline(admin.TabularInline):
+    model = ItemTreino
+    extra = 1 # Quantos campos vazios aparecem por padrão
+
+@admin.register(Treino)
+class TreinoAdmin(admin.ModelAdmin):
+    list_display = ('user', 'goal', 'name', 'created_at')
+    list_filter = ('goal', 'user')
+    search_fields = ('user__username', 'name')
+
+# @admin.register(TreinoExercicio)
+# class TreinoExercicioAdmin(admin.ModelAdmin):
+#     list_display = ('treino', 'exercicio', 'order')
+#     list_filter = ('treino__goal',)
+#     search_fields = ('treino__user__username', 'exercicio__name')
--- workoutengine/models.py
+++ workoutengine/models.py
@@ -1,15 +1,31 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
 
 class Exercicio(models.Model):
+
     MUSCLE_GROUPS_CHOICES = [
-        ('chest', 'Peito'),
-        ('back', 'Costas'),
-        ('legs', 'Pernas'),
-        ('shoulders', 'Ombros'),
-        ('biceps', 'Bíceps'),
-        ('triceps', 'Tríceps'),
+        ('ches_upper', 'Peito (Superior)'),
+        ('chest_lower', 'Peito (Inferior)'),
+        ('chest_middle', 'Peito (Médio)'),
+        ('back_upper', 'Costas (Parte Superior)'),
+        ('back_lats', 'Costas (Grande Dorsal)'),
+        ('back_middle', 'Costas (Miolo)'),
+        ('back_lower', 'Costas (Parte Inferior)'),
+        ('legs_quads', 'Pernas (Quadríceps)'),
+        ('legs_hamstrings', 'Pernas (Posterior)'),
+        ('legs_glutes', 'Pernas (Glúteos)'),
+        ('legs_calves', 'Pernas (Panturrilhas)'),
+        ('shoulders_ant', 'Ombros (Anterior)'),
+        ('shoulders_med', 'Ombros (Médio)'),
+        ('shoulders_post', 'Ombros (Posterior)'),
+        ('biceps_long', 'Bíceps (Cabeça Longa)'),
+        ('biceps_short', 'Bíceps (Cabeça Curta)'),
+        ('triceps_long', 'Tríceps (Cabeça Longa)'),
+        ('triceps_middle', 'Tríceps (Cabeça Medial)'),
+        ('triceps_lat', 'Tríceps (Cabeça Lateral)'),
+
         ('abs', 'Abdômen'),
     ]
 
@@ -32,3 +48,37 @@ class Exercicio(models.Model):
 
     def __str__(self):
         return f"{self.name} - {self.get_muscle_group_display()}"
+    
+class Treino(models.Model):
+    GOAL_CHOICES = [
+        ('hipertrofia', 'Hipertrofia'),
+        ('resistencia', 'Resistência'),
+        ('forca', 'Força'),
+        ('emagrecimento', 'Emagrecimento'),
+    ]
+
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100, verbose_name="Nome do Treino")
+    created_at = models.DateTimeField(auto_now_add=True)
+    goal = models.CharField(max_length=20, choices=GOAL_CHOICES, verbose_name="Objetivo do Treino", default='hipertrofia')
+
+    class Meta:
+        verbose_name = "Treino"
+        verbose_name_plural = "Treinos"
+
+    def __str__(self):
+        return f"{self.name} - {self.user.email}"
+        
+class ItemTreino(models.Model):
+    treino = models.ForeignKey(Treino, on_delete=models.CASCADE, related_name='itens')
+    exercicio = models.ForeignKey(Exercicio, on_delete=models.CASCADE)
+    sets = models.IntegerField()
+    reps = models.IntegerField()
+    rest_time = models.IntegerField(help_text="Tempo de descanso entre as séries (em segundos)")
+
+    class Meta:
+        verbose_name = "Item de Treino"
+        verbose_name_plural = "Itens de Treino"
+
+    def __str__(self):
+        return f"{self.exercicio.name} - {self.sets}x{self.reps} (Descanso: {self.rest_time}s)"
